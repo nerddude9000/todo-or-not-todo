561a7c8110eb3330e126bcfa7acaf35de12a6c74
Mohammed Akram <nerddude9000@proton.me>
2026-05-01 16:22:56 +0100

    feat: header and bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import "./App.css";
 
 export default function App() {
-  return <h1 className="italic">hello</h1>;
+  return (
+    <>
+      <header className="w-screen h-16 flex items-center justify-center bg-black">
+        <a
+          href="https://github.com/nerddude9000/todo-or-not-todo"
+          target="_blank"
+          className="font-light italic text-lg"
+        >
+          To do, or not to do...
+        </a>
+      </header>
+    </>
+  );
 }
